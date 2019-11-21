17bcbf7c96110b249209d0d1cf831ec83f027a37
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2019-11-21 10:16:53 +0500

    Add files via upload

--- TestLib/Class1.cs
+++ TestLib/Class1.cs
@@ -1,1 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TestLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestLib")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2f124775-6a91-436a-9a67-7a598156f194")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+namespace TestLib
+{
+    public static class Class1
+    {
+        public static double CalcSum(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static double CalcDiv(double x, double y)
+        {
+            if (y == 0.0) throw new DivideByZeroException();
+            return x / y;
+        }
+
+    }
+}
